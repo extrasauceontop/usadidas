a30507c90ad597b01cac6bae9c1a4cfbc741ca9d
extrasauceontop <msuchemba@gmail.com>
2021-03-02 13:46:57 -0500

    commit

--- scrape.py
+++ scrape.py
@@ -22,7 +22,7 @@ def fetch_data():
     }
     r = session.get("https://placesws.adidas-group.com/API/search?brand=adidas&geoengine=google&method=get&category=store&latlng=39.491751305282705%2C-471.94216860348604%2C15000&page=1&pagesize=50000&fields=name%2Cstreet1%2Cstreet2%2Caddressline%2Cbuildingname%2Cpostal_code%2Ccity%2Cstate%2Cstore_o+wner%2Ccountry%2Cstoretype%2Clongitude_google%2Clatitude_google%2Cstore_owner%2Cstate%2Cperformance%2Cbrand_store%2Cfactory_outlet%2Coriginals%2Cneo_label%2Cy3%2Cslvr%2Cchildren%2Cwoman%2Cfootwear%2Cfootball%2Cbasketball%2Coutdoor%2Cporsche_design%2Cmiadidas%2Cmiteam%2Cstella_mccartney%2Ceyewear%2Cmicoach%2Copening_ceremony%2Coperational_status%2Cfrom_date%2Cto_date%2Cdont_show_country&format=json&storetype=",headers=headers)
     r = r.text.replace("\n", "").replace("\r", "").encode("utf-8")
-    soup = BeautifulSoup(r.text,'html.parser')
+    soup = BeautifulSoup(r,'html.parser')
     location_data = json.loads(str(soup).split("<body>")[1].split("</body>")[0].replace("<br>","").replace("\n",""))['wsResponse']["result"]
     for store_data in location_data:
         if store_data["country"] == "US" or store_data["country"] == "CA":
